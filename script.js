98716e5e50fd97f0f87ee95f2ae78e06ccb7c918
bwg0979 <Bwg0979@autuni.ac.nz>
2024-06-24 16:56:03 +1200

    Temp Hold

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-import { config } from "dotenv";
-config()
+import OpenAI from "openai";
 
-console.log(process.env.API_KEY)
+const openai = new OpenAI({
+    organization: "org-org-u3J0qaQeaUSb0Q0UfnyrX9SQ",
+    project: "$proj_S8pyLO3R8aaf67WrFkgNkgk9",
+});
+
+curl https://api.openai.com/v1/chat/completions \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer sk-proj-QjYL9AmyqUe1dM0Ge6OLT3BlbkFJJNtGw45LT8v2G8nNMYys" \
+  -d '{
+     "model": "gpt-3.5-turbo",
+     "messages": [{"role": "user", "content": "Say this is a test!"}],
+     "temperature": 0.7
+   }'
