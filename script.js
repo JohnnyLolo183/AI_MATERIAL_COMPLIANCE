a9114649ee119ddd40ff2faf589b818220735fdc
bwg0979 <Bwg0979@autuni.ac.nz>
2024-06-24 17:32:24 +1200

    cc

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ const openai = new OpenAI({
     project: "$proj_S8pyLO3R8aaf67WrFkgNkgk9",
 });
 
-curl https://api.openai.com/v1/chat/completions \
-  -H "Content-Type: application/json" \
-  -H "Authorization: Bearer sk-proj-QjYL9AmyqUe1dM0Ge6OLT3BlbkFJJNtGw45LT8v2G8nNMYys" \
-  -d '{
-     "model": "gpt-3.5-turbo",
-     "messages": [{"role": "user", "content": "Say this is a test!"}],
-     "temperature": 0.7
-   }'
+// curl https://api.openai.com/v1/chat/completions \
+//  -H "Content-Type: application/json" \
+// -H "Authorization: Bearer sk-proj-QjYL9AmyqUe1dM0Ge6OLT3BlbkFJJNtGw45LT8v2G8nNMYys" \
+//  -d '{
+//    "model": "gpt-3.5-turbo",
+//    "messages": [{"role": "user", "content": "Say this is a test!"}],
+//    "temperature": 0.7
+//   }' ``
